19ee5cff9002d3009b0a72f8e820abdf91ffda20
oran moyal <oranmoyal2@gmail.com>
2023-01-15 20:53:14 +0200

    GetCorrelatedLine added

--- flightsimulator/src/main/java/com/example/model/anomalies/TimeSeriesAnomalyDetector.java
+++ flightsimulator/src/main/java/com/example/model/anomalies/TimeSeriesAnomalyDetector.java
@@ -1,9 +1,14 @@
 package com.example.model.anomalies;
 
 import java.util.List;
+import com.example.util.*;
 
 public interface TimeSeriesAnomalyDetector {
 	void learnNormal(TimeSeries ts);
 
 	List<AnomalyReport> detect(TimeSeries ts);
+
+	Line GetCorrelatedLine(String attr1, String attr2);
+
+	
 }
